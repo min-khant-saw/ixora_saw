98270fd61bc7e7bf87a1454640adce247707e816
Min Khant Saw <minkhantsaw04@gmail.com>
2023-09-17 13:31:15 +0630

    category

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,24 +37,24 @@
             <div class="relative overflow-hidden fashion">
                 <div class="absolute top-0 left-0 text-white fashion-text flex justify-center items-center flex-col" style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.696); transition: all .4s ease-in-out; margin-top: 300px">
                     <h1 class="text-2xl font-semibold category-head">Fashion</h1>
-                    <div class="flex justify-center items-center transition-all shop-now-container">
+                    <a href="{{route('fashion')}}" class="flex justify-center items-center transition-all shop-now-container">
                         <p class="shop-now">Shop Now </p>
                         <span class="material-symbols-rounded mt-1" style="font-size: 18px">
                             arrow_forward
                             </span>
-                    </div>
+                    </a>
                 </div>
                 <img class="category-img" style="width: 250px; height:250px; object-fit: cover;" src="https://scontent-kul3-1.xx.fbcdn.net/v/t39.30808-6/377931717_619740620279329_7494400302942887383_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=49d041&_nc_ohc=uGPpD29QvQcAX_00wwB&_nc_ht=scontent-kul3-1.xx&oh=00_AfAHfbSfN6ldAuKcFiWKoVoFcE_ssRp7dH3roRV-1fwoUg&oe=650B9C03" alt="">
             </div>
             <div class="relative overflow-hidden fashion">
                 <div class="absolute top-0 left-0 text-white fashion-text flex justify-center items-center flex-col" style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.696); transition: all .4s ease-in-out; margin-top: 300px">
                     <h1 class="text-2xl font-semibold category-head">Accessories</h1>
-                    <div class="flex justify-center items-center transition-all shop-now-container">
+                    <a href="{{route('accessories')}}" class="flex justify-center items-center transition-all shop-now-container">
                         <p class="shop-now">Shop Now </p>
                         <span class="material-symbols-rounded mt-1" style="font-size: 18px">
                             arrow_forward
                             </span>
-                    </div>
+                    </a>
                 </div>
                 <img class="category-img" style="width: 250px; height:250px; object-fit: cover;" src="https://hips.hearstapps.com/hmg-prod/images/wireless-earbuds-1661183784.jpg?resize=1200:*" alt="">
             </div>
@@ -62,12 +62,12 @@
             <div class="relative overflow-hidden fashion">
                 <div class="absolute top-0 left-0 text-white fashion-text flex justify-center items-center flex-col" style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.696); transition: all .4s ease-in-out; margin-top: 300px">
                     <h1 class="text-2xl font-semibold category-head">Home Decors</h1>
-                    <div class="flex justify-center items-center transition-all shop-now-container">
+                    <a href="{{route('homeDecor')}}" class="flex justify-center items-center transition-all shop-now-container">
                         <p class="shop-now">Shop Now </p>
                         <span class="material-symbols-rounded mt-1" style="font-size: 18px">
                             arrow_forward
                             </span>
-                    </div>
+                    </a>
                 </div>
                 <img class="category-img" style="width: 250px; height:250px; object-fit: cover;" src="https://media.designcafe.com/wp-content/uploads/2022/09/16092055/cheap-room-decor-ideas.jpg" alt="">
             </div>
