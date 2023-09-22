9040945e2cf35bfe5c3db3a658ac70f652e5ae25
Min Khant Saw <minkhantsaw04@gmail.com>
2023-09-22 13:44:30 +0630

    style

--- resources/views/header/html-header.blade.php
+++ resources/views/header/html-header.blade.php
@@ -4,7 +4,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-<link rel="stylesheet" href="{{asset("style.css")}}">
+<link rel="stylesheet" href="{{asset("a.css")}}">
 
 
         <!-- Fonts -->
