4c0ee9868c293c98cf4aca12873601178982e3d7
Min Khant Saw <minkhantsaw04@gmail.com>
2023-09-24 14:45:19 +0630

    js

--- public/js/script.js
+++ public/js/script.js
@@ -91,13 +91,9 @@ imageContainer.addEventListener(events[deviceType].move, (e) => {
     mouseOverlay.style.top = y + "px";
     mouseOverlay.style.left = x + "px";
     overlay.style.top = y + "px";
-    if (window.screen.width < 1001) {
-        overlay.style.left = x - 310 + "px";
-    } else {
+    if (window.screen.width > 1000) {
         overlay.style.left = x + "px";
+    } else {
+        overlay.style.left = x - 310 + "px";
     }
-
-    window.onresize(() => {
-        if (window.screen.width < 1001) overlay.style.left = x - 310 + "px";
-    });
 });
