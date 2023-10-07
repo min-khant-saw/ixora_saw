ad1fb4220fbe5ea1a800e824accce40e0c0fd405
Min Khant Saw <minkhantsaw04@gmail.com>
2023-10-07 14:07:14 +0630

    site name

--- resources/views/layouts/app-guest-layout.blade.php
+++ resources/views/layouts/app-guest-layout.blade.php
@@ -2,21 +2,26 @@
 @include('header.html-header')
 <meta property="og:site_name" content="Ixora Saw">
 <meta property="og:description" content="Boutique. Sell handmade products.">
-<meta property="og:image" content="URL to your image">
-<meta property="og:url" content="URL of your page">
-<meta property="og:type" content="website">
+<meta property="og:image" content="{{ URL::asset('build/assets/images/Ixora-Saw-Logo-background.png') }}">
+<meta property="og:url" content="{{ url()->full() }}">
+<meta property="og:type" content="Ecommerce">
+
 <body>
     @include('layouts.navigation')
-    <div class="w-full text-center justify-center items-center banner {{request()->routeIs('home')? 'flex': 'hidden'}}" style="background-color: black;">
-        <span class="" style="color: white; font-weight: 600;">Only for sale 75$ </span> <a href="" class="rounded font-semibold ml-3" style="border: 2px; background-color: white; color: black">Buy Now <span class="">!</span></a>
+    <div class="w-full text-center justify-center items-center banner {{ request()->routeIs('home') ? 'flex' : 'hidden' }}"
+        style="background-color: black;">
+        <span class="" style="color: white; font-weight: 600;">Only for sale 75$ </span> <a href=""
+            class="rounded font-semibold ml-3" style="border: 2px; background-color: white; color: black">Buy Now <span
+                class="">!</span></a>
     </div>
     <div class="min-h-screen">
         {{ $slot }}
     </div>
     @include('footer')
-             {{-- script --}}
-             <script src="{{asset('js/s.js')}}"></script>
-             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    {{-- script --}}
+    <script src="{{ asset('js/s.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
