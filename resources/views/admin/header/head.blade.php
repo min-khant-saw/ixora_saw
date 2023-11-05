a79833f4a87c8bdb9e434289634f1a7b0729df95
Min Khant Saw <minkhantsaw04@gmail.com>
2023-11-05 16:00:22 +0630

    footer image

--- resources/views/admin/header/head.blade.php
+++ resources/views/admin/header/head.blade.php
@@ -6,7 +6,7 @@
     <title>{{ config('app.name') }}</title>
 
     <!-- <link rel="stylesheet" href="{{ asset('mystyle.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('ms.css') }}">
+    <link rel="stylesheet" href="{{ asset('s.css') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
